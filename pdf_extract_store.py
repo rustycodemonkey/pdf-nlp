a3a8ea3ed508fc83315ec619b81f2db618b49b25
Phillip Kim <philkim78@gmail.com>
2019-05-16 17:35:36 +1000

    Extracting all pdf files in data directory

---
+++ pdf_extract_store.py
@@ -0,0 +1,66 @@
+import sys
+#import glob
+import pdfminer.settings
+import pdfminer.high_level
+import pdfminer.layout
+from pdfminer.image import ImageWriter
+pdfminer.settings.STRICT = False
+
+
+def extract_text(parameters=[], outfile='-',
+            _py2_no_more_posargs=None,  # Bloody Python2 needs a shim
+            no_laparams=False, all_texts=None, detect_vertical=None, # LAParams
+            word_margin=None, char_margin=None, line_margin=None, boxes_flow=None, # LAParams
+            output_type='text', codec='utf-8', strip_control=False,
+            maxpages=0, page_numbers=None, password="", scale=1.0, rotation=0,
+            layoutmode='normal', output_dir=None, debug=False,
+            disable_caching=False, **other):
+    if _py2_no_more_posargs is not None:
+        raise ValueError("Too many positional arguments passed.")
+    if not parameters:
+        raise ValueError("Must provide files to work upon!")
+
+    # If any LAParams group arguments were passed, create an LAParams object and
+    # populate with given args. Otherwise, set it to None.
+    if not no_laparams:
+        laparams = pdfminer.layout.LAParams()
+        for param in ("all_texts", "detect_vertical", "word_margin", "char_margin", "line_margin", "boxes_flow"):
+            paramv = locals().get(param, None)
+            if paramv is not None:
+                setattr(laparams, param, paramv)
+    else:
+        laparams = None
+
+    imagewriter = None
+    if output_dir:
+        imagewriter = ImageWriter(output_dir)
+
+    if output_type == "text" and outfile != "-":
+        for override, alttype in (  (".htm", "html"),
+                                    (".html", "html"),
+                                    (".xml", "xml"),
+                                    (".tag", "tag") ):
+            if outfile.endswith(override):
+                output_type = alttype
+
+    if outfile == "-":
+        outfp = sys.stdout
+        if outfp.encoding is not None:
+            codec = 'utf-8'
+    else:
+        outfp = open(outfile, "wb")
+
+    print(parameters['files'])
+
+    for fname in parameters['files']:
+        with open(fname, "rb") as fp:
+            pdfminer.high_level.extract_text_to_fp(fp, **locals())
+    return outfp
+
+def main(args=None):
+    # parameters = {'files': glob.glob("data/*.pdf")}
+    parameters = {'files': ['data/active_index_income_fund.pdf']}
+    extract_text(parameters)
+
+if __name__ == '__main__':
+    sys.exit(main())
